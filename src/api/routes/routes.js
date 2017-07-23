9cee63917a7a3a9f72ddb811abd3ac36fdfae5f1
Kunle Ogunjimi <kdotkid3@gmail.com>
2017-07-23 23:38:30 +0100

    understanding routers and logging request

---
+++ src/api/routes/routes.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = function(app, exampleRoute) {
+    app.route("/student")
+    .get(function(req, res, next) {
+       res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+    })
+
+    exampleRoute.use(function(req, res, next) {
+        res.json({notes: "we in our used method"});
+        console.log("we outputting this " + req.method, "the requested url is this: " + req.url);
+        next();
+    });
+
+    exampleRoute.get('/events', function(req, res, next) {
+        res.json({notes: "we in our used method inside events http"})
+    });
+
+    app.use('/', exampleRoute);
+}
--- src/api/server.js
+++ src/api/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const app     = express();
+const express       = require('express'),
+app                 = express(),
+bodyParser          = require('body-parser');
 
-var port      = 3000;
+var routes          = require('./routes/routes'),
+exampleRoute    = require('express').Router(),
+port                = 3000;
 
-app.get('/', function(req, res){
-    res.send('Hello world')
-});
+// app.get('/', function(req, res){
+//     res.send('Hello world')
+// });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app, exampleRoute);
 
 app.listen(port, function(){
-    console.log('listening to port: ' + port);
+    console.log('listening to http://localhost:' + port);
 });
